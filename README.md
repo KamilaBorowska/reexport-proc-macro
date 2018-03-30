63d3fc255c34e66ca5322ff907a10441d599b4cf
Konrad Borowski <github@borowski.pw>
2018-03-30 15:44:28 +0200

    Add proper Cargo.toml

---
+++ README.md
@@ -0,0 +1,16 @@
+# `reexport-proc-macro`
+
+[![Build Status](https://travis-ci.org/xfix/reexport-proc-macro.svg?branch=master)](https://travis-ci.org/xfix/reexport-proc-macro)
+
+Allows re-exporting a procedural macro while adding additional public items.
+
+## Examples
+
+This makes a crate export all serde derives.
+
+```
+#[macro_use]
+extern crate reexport_proc_macro;
+reexport_proc_macro!(serde_derive);
+# fn main() {}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 //! A crate providing a macro to re-export a procedural macro.
-//! 
-//! This allows for adding additional public items in addition to 
+//!
+//! This allows for adding additional public items in addition to
 //! re-exported derives.
 
 #![no_std]
 
 /// Re-exports a procedural macro so that all its derives are visible
 /// publicly while allowing to add additional public items.
-/// 
+///
 /// Intended to be only used with proc-macro crates.
 ///
 /// # Examples
 ///
 /// This makes a crate export all serde derives.
-/// 
+///
 /// ```
 /// #[macro_use]
 /// extern crate reexport_proc_macro;
